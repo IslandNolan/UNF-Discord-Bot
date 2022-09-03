c81f11beaab72c0ac2ef8c97fb969cca5e180981
Noaln <Nolanboner@icloud.com>
2022-09-03 16:01:35 -0400

    Simple Jackson Yaml Parsing for key/token File.

--- Main/src/main/java/dev/noaln/Main.java
+++ Main/src/main/java/dev/noaln/Main.java
@@ -1,7 +1,31 @@
 package dev.noaln;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import dev.noaln.classes.Credential;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+
 public class Main {
+
+    private static Credential credFile = null;
+    private static final Yaml yaml = new Yaml();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //TODO: Proper Logging Library Setup
+        System.out.println("Loading Credentials.. ");
+        try{
+            ObjectMapper mapper = new ObjectMapper(new YAMLFactory()); // jackson databind
+            credFile = mapper.readValue(new File("credentials.yaml"), Credential.class);
+
+            System.out.println("Canvas: "+credFile.getCanvasapitoken());
+            System.out.println("Engage: "+credFile.getEngageapitoken());
+            System.out.println("Discord: "+credFile.getDiscordapitoken());
+
+        }
+        catch (Exception E){
+            E.printStackTrace();
+        }
     }
 }
---
+++ Main/src/main/java/dev/noaln/classes/Credential.java
@@ -0,0 +1,31 @@
+package dev.noaln.classes;
+
+public class Credential {
+    private String discordapitoken;
+    private String canvasapitoken;
+    private String engageapitoken;
+
+    public String getDiscordapitoken() {
+        return discordapitoken;
+    }
+
+    public void setDiscordapitoken(String discordapitoken) {
+        this.discordapitoken = discordapitoken;
+    }
+
+    public String getCanvasapitoken() {
+        return canvasapitoken;
+    }
+
+    public void setCanvasapitoken(String canvasapitoken) {
+        this.canvasapitoken = canvasapitoken;
+    }
+
+    public String getEngageapitoken() {
+        return engageapitoken;
+    }
+
+    public void setEngageapitoken(String engageapitoken) {
+        this.engageapitoken = engageapitoken;
+    }
+}
